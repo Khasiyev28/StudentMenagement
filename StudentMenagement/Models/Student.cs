4260c8f6189a7cf2dcce0e82ea7f5696c0165e29
Khasiyev28 <mahirkhasiyev28@gmail.com>
2026-03-17 21:40:26 +0400

    .

--- StudentMenagement/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentMenagement.Models
-{
-    public class Student
-    {
-        public string StudentNumber { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-    }
-}
